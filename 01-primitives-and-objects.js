42a2583f28a367975e5cfa401a9449c0edfca79d
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-27 13:52:39 +0530

    added 01

---
+++ 01-primitives-and-objects.js
@@ -0,0 +1,12 @@
+//nn bb ss u
+let a=null
+let b=345
+let c=true;
+let d=BigInt("345")+BigInt("5")
+let e="vaibhav"
+let f=Symbol("I am vaibhav")
+let g=undefined // can also be written as (let g)
+
+console.log(a,b,c,d,e,f,g)
+
+console.log(typeof d) // to find type of a variable
