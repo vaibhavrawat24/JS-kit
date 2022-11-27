ceadf4593eea1e19d756bb18730eff201d3d9993
Vaibhav <rawatvaibhav42@gmail.com>
2022-11-27 13:58:55 +0530

    edited 01

--- 01-primitives-and-objects.js
+++ 01-primitives-and-objects.js
@@ -1,4 +1,6 @@
-//nn bb ss u
+//nn bb ss u - primitive data types
+//null number boolean bigint string symbol undefined
+
 let a=null
 let b=345
 let c=true;
@@ -10,3 +12,14 @@ let g=undefined // can also be written as (let g)
 console.log(a,b,c,d,e,f,g)
 
 console.log(typeof d) // to find type of a variable
+
+
+//Objects - non primitive data types
+
+const item={
+    "Vaibhav":"yo",
+    "harry":98
+}
+
+console.log(item["Vaibhav"])
+console.log(item["harry"])
