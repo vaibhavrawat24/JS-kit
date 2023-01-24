6ae5d20641af930ba37187f67de93bb5c6ad033b
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-24 14:59:53 +0530

    edited 04

--- 04-loops.js
+++ 04-loops.js
@@ -1,10 +1,10 @@
-var arr=[1,2,3,4,5]
-// console.log(arr)
+// var arr=[1,2,3,4,5]
+// // console.log(arr)
 
-for(var i=0;i<arr.length;i++)
-{
-    console.log(arr[i])
-}
+// for(var i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i])
+// }
 
 //another way to use for loop
 
@@ -29,3 +29,17 @@ for(var i=0;i<arr.length;i++)
 //     console.log(arr[j])
 //     j+=1
 // }while(j<arr.length);
+
+
+let myarr=["vaibhav","camera","joey",null,true]
+
+//Array methods
+
+// myarr.pop();
+// myarr.push("chandy")
+
+// myarr.shift() //skips first element
+
+myarr.unshift("joey") //brings desired element to first 
+
+console.log(myarr)
