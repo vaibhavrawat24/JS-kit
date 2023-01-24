286334af166235f97fd21009470e5bf580342aee
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-24 12:47:24 +0530

    edited file 02

--- 02-operators-and-expressions.js
+++ 02-operators-and-expressions.js
@@ -2,3 +2,13 @@ let a=45
 let b=32
 
 console.log("a**b",a**b) //a to the power b
+
+//Comparison Operator
+
+let comp1=6
+let comp2="6"
+
+console.log("comp1==comp2",comp1==comp2)
+console.log("comp1===comp2",comp1===comp2)
+console.log("comp1!=comp2",comp1!=comp2)
+console.log("comp1!==comp2",comp1!==comp2)
