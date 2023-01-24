dc0575bde7b11971f34b2646121d05db5700f99b
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-24 14:47:45 +0530

    edited 04

--- 04-loops.js
+++ 04-loops.js
@@ -1,7 +1,14 @@
 var arr=[1,2,3,4,5]
-console.log(arr)
+// console.log(arr)
 
-for(var i=0;i<arr.length;i++)
+// for(var i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i])
+// }
+
+//another way to use for loop
+
+arr.forEach(function(element)
 {
-    console.log(arr[i])
-}
+    console.log(element)
+})
