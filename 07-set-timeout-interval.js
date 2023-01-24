79ffbdf3346afb07bf43c40c18e09b2ba0f07acb
Vaibhav <rawatvaibhav42@gmail.com>
2023-01-24 15:45:52 +0530

    added 07

---
+++ 07-set-timeout-interval.js
@@ -0,0 +1,20 @@
+//Arrow functions
+
+// function sum(a,b)
+// {
+//     return a+b
+// }
+
+//can also be written as
+
+// sum=(a,b)=>{
+//     return a+b
+// }
+
+//Set timeout and setinterval
+
+logkaro=()=>{
+    console.log("Im your log")
+}
+
+setTimeout(logkaro,2000)
